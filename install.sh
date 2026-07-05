302fc3b02d537f57a0dc44b1b93391ea41f2c5c2
ysktl <ysktl@noreply.localhost>
2026-07-05 16:44:20 +0900

    fix install command: lazyjj

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ else if [ "$ID" == "arch" ]; then
   sudo pacman -S --noconfirm git vim github-cli base-devel
   git clone https://aur.archlinux.org/paru.git
   cd paru && makepkg -si
-  paru -S --noconfirm neovim noctalia-shell zsh rustup ghostty zed helium-browser-bin zellij-git yazi lazygit fcitx5-im fcitx5-mozc noto-fonts-cjk wl-clipboard jujutsu
+  paru -S --noconfirm neovim noctalia-shell zsh rustup ghostty zed helium-browser-bin zellij-git yazi lazygit fcitx5-im fcitx5-mozc noto-fonts-cjk wl-clipboard jujutsu lazyjj
 else if [ "$ID" == "opensuse-tumbleweed" ]; then
   sudo zypper addrepo https://download.opensuse.org/repositories/home:itachi_re/openSUSE_Tumbleweed/home:itachi_re.repo
   sudo zypper refresh
@@ -60,7 +60,6 @@ sudo echo "ja_JP.UTF-8 UTF-8" | sudo tee /etc/locale.gen
 sudo locale-gen
 fc-cache -fv
 curl -fsSL https://herdr.dev/install.sh | sh
-curl -fsSL https://lazyjj.dev/install.sh | bash
 
 # Nerd fonts
 
