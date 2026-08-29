11c517d754cdc9e894b6797114dc1337fc00ee22
ysktl <ysktl@noreply.localhost>
2026-08-29 14:15:49 +0900

    fix commands

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # Distro
 
 source /etc/os-release
@@ -7,8 +9,8 @@ source /etc/os-release
 echo "Initializing installation...."
 
 if [ "$ID" == "debian" ]; then
-  if ! command -v nix-channel >/dev/null 2>&1; then
-    sudo apt install -y curl wget git gh vim zsh xz-utils libatomic1 make cmake keychain uidmap usbutils dbus-user-session
+  if ! command -v nix >/dev/null 2>&1; then
+    sudo apt install -y curl wget unzip gh vim zsh xz-utils libatomic1 make cmake keychain uidmap usbutils dbus-user-session fontconfig
     sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
     echo "Installed nix, Run ./install.sh again after restarting"
     exit 0
@@ -18,28 +20,28 @@ if [ "$ID" == "debian" ]; then
     nix-channel --update
     nix-shell '<home-manager>' -A install
     sudo mkdir -p /etc/nix
-    sudo echo "experimental-features = nix-command flakes" | sudo tee /etc/nix/nix.conf
+    echo "experimental-features = nix-command flakes" | sudo tee /etc/nix/nix.conf
     sudo systemctl restart nix-daemon
-    cd ~/nix && home-manager switch --flake .
-    sudo systemctl start dbus --user
+    cd ~/dotfiles/home/nix && home-manager switch --flake .
+    systemctl start dbus --user
     sudo mkdir -p /etc/containers
-    sudo echo '{ "default": [ { "type": "insecureAcceptAnything" } ], "transports": {} }' | sudo tee /etc/containers/policy.json
+    echo '{ "default": [ { "type": "insecureAcceptAnything" } ], "transports": {} }' | sudo tee /etc/containers/policy.json
     sudo mount --make-rshared /
-    sudo echo 'unqualified-search-registries = ["docker.io"]' | sudo tee /etc/containers/registries.conf
+    echo 'unqualified-search-registries = ["docker.io"]' | sudo tee /etc/containers/registries.conf
   fi
-else if [ "$ID" == "arch" ]; then
+elif [ "$ID" == "arch" ]; then
   sudo pacman -S --noconfirm git vim github-cli base-devel
   git clone https://aur.archlinux.org/paru.git
   cd paru && makepkg -si
   paru -S --noconfirm neovim noctalia-shell zsh rustup ghostty zed helium-browser-bin yazi lazygit fcitx5-im fcitx5-mozc noto-fonts-cjk wl-clipboard jujutsu lazyjj
-else if [ "$ID" == "opensuse-tumbleweed" ]; then
+elif [ "$ID" == "opensuse-tumbleweed" ]; then
   sudo zypper addrepo https://download.opensuse.org/repositories/home:itachi_re/openSUSE_Tumbleweed/home:itachi_re.repo
   sudo zypper refresh
   sudo zypper install lua51-lpeg git vim gh neovim zsh rustup yazi lazygit ghostty fcitx5-mozc
-else if [ "$ID" == "ferdora" ]; then
-
-else if [ "$ID" == "void" ]; then
-
+elif [ "$ID" == "fedora" ]; then
+  :
+elif [ "$ID" == "void" ]; then
+  :
 fi
 
 echo "Finished initializing."
@@ -51,21 +53,22 @@ echo "Starting common packages installation..."
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply ysktl
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 curl -sS https://starship.rs/install.sh | sh
-chsh -s $(which zsh)
+chsh -s $(command -v zsh)
 rustup default nightly
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 curl https://mise.run | sh
 mise use -g node npm bun deno uv pkl k9s
 git clone https://github.com/ysktl/nvim.git ~/.config/nvim/
-sudo echo "ja_JP.UTF-8 UTF-8" | sudo tee /etc/locale.gen
+echo "ja_JP.UTF-8 UTF-8" | sudo tee /etc/locale.gen
 sudo locale-gen
 fc-cache -fv
 curl -fsSL https://herdr.dev/install.sh | sh
-bash <(curl -L https://zellij.dev/launch)
+cargo install --locked zellij
 
 # Nerd fonts
 
-fons_list=("Go-Mono" "Hack" "JetbrainsMono")
-select font_name in "${fons_list[@]}" "Quit";
+fonts_list=("Go-Mono" "Hack" "JetbrainsMono")
+select font_name in "${fonts_list[@]}" "Quit";
   do
     if [ "$(command -v curl)" ]; then
       if [ -n "$font_name" ]; then
